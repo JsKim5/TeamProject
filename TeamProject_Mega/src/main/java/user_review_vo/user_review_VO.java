04c9303907cb772f3817cb82513f11b4fbb88a5a
parkhyunchung <linda@naver.com>
2023-11-09 15:29:24 +0900

    commit

---
+++ TeamProject_Mega/src/main/java/user_review_vo/user_review_VO.java
@@ -0,0 +1,28 @@
+package user_review_vo;
+
+public class user_review_VO {
+	
+	int review_idx;
+	double user_score;
+	String user_review;
+	
+	public int getReview_idx() {
+		return review_idx;
+	}
+	public void setReview_idx(int review_idx) {
+		this.review_idx = review_idx;
+	}
+	public double getUser_score() {
+		return user_score;
+	}
+	public void setUser_score(double user_score) {
+		this.user_score = user_score;
+	}
+	public String getUser_review() {
+		return user_review;
+	}
+	public void setUser_review(String user_review) {
+		this.user_review = user_review;
+	}
+
+}

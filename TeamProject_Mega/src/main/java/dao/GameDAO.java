c4625badccaea10496178961e820f8a49ef4e557
Joonsung <sju07326@naver.com>
2023-12-06 15:15:04 +0900

    1

--- TeamProject_Mega/src/main/java/dao/GameDAO.java
+++ TeamProject_Mega/src/main/java/dao/GameDAO.java
@@ -333,10 +333,6 @@ public class GameDAO {
 		return res;
 	}
 
-<<<<<<< HEAD
-=======
-
->>>>>>> refs/heads/JoonSung
 	public int mainPageInsert(GameVO vo) {
 		int res = sqlSession.insert("g.mainPageInsert", vo);
 		return res;
